7456e7d76e49f08c8283c10d37868f2c78393247
quylhph28754 <quylhph28754@fpt.edu.vn>
2024-08-18 19:31:41 +0700

    Update Constants

--- app/src/main/java/com/fpoly/shoes_app/utility/Constans.kt
+++ app/src/main/java/com/fpoly/shoes_app/utility/Constans.kt
@@ -3,7 +3,7 @@ package com.fpoly.shoes_app.utility
 
 class Constants {
     companion object {
-        const val BASE_URL = "http://192.168.1.7:8000/api/"
+        const val BASE_URL = "http://192.168.1.7:3000/api/"
         const val SET_TIME_OUT_API = 100L
     }
 }
